9453bb1398dc6bbf0d55ef0ec0288774bc42ae0a
David Hodge <dhodge@apple.com>
2016-06-29 11:09:34 -0700

    Initial Commit

---
+++ FPSTester/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FPSTester
+//
+//  Created by David Hodge on 6/29/16.
+//  Copyright © 2016 David Hodge. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FPSTester/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  FPSTester
+//
+//  Created by David Hodge on 6/29/16.
+//  Copyright © 2016 David Hodge. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
